5a8422ed9fd3f203dce4862e5c2b4cdd16778a85
Simon Génier <1782845+animalsiknow@users.noreply.github.com>
2018-04-13 23:42:49 -0400

    Replace put with get_or_insert.

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,8 @@ impl<T> LazyArray<T> {
     LazyArray(cell::UnsafeCell::new(inner))
   }
 
-  pub fn put(&self, index: usize, t: T) {
-    match &mut unsafe { &mut *self.0.get() }[index] {
-      empty @ &mut None => *empty = Some(t),
-      _ => (),
-    }
+  pub fn get_or_insert(&self, index: usize, t: T) -> &T {
+    (&mut unsafe { &mut *self.0.get() }[index]).get_or_insert(t)
   }
 
   pub fn get(&self, index: usize) -> Option<&T> {
@@ -40,7 +37,7 @@ mod tests {
       assert_eq!(lazy_array.get(i), None)
     }
 
-    lazy_array.put(7, 112233);
+    assert_eq!(lazy_array.get_or_insert(7, 112233), &112233);
 
     for i in 0..10 {
       assert_eq!(lazy_array.get(i), if i == 7 { Some(&112233) } else { None })
@@ -48,9 +45,16 @@ mod tests {
   }
 
   #[test]
+  fn cannot_insert_twice() {
+    let lazy_array = LazyArray::<i32>::new(10);
+    assert_eq!(lazy_array.get_or_insert(7, 112233), &112233);
+    assert_eq!(lazy_array.get_or_insert(7, 445566), &112233);
+  }
+
+  #[test]
   #[should_panic]
   fn cannot_put_out_of_bounds() {
     let lazy_array = LazyArray::<i32>::new(10);
-    lazy_array.put(10, 112233);
+    lazy_array.get_or_insert(10, 112233);
   }
 }
