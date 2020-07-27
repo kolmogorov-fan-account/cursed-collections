c23e775bfe6b58262840e495a65e4748dc20fca2
Simon Génier <simon.genier@protonmail.com>
2020-07-27 18:27:03 -0400

    Indent with 4 spaces.

--- src/append_only_vec.rs
+++ src/append_only_vec.rs
@@ -5,53 +5,53 @@ const SEGMENT_CAPACITY: usize = 1 << SEGMENT_CAPACITY_LOG_2;
 const SEGMENT_CAPACITY_MASK: usize = SEGMENT_CAPACITY - 1;
 
 struct Segment<T> {
-  len: usize,
-  elements: Box<[mem::ManuallyDrop<T>; SEGMENT_CAPACITY]>,
+    len: usize,
+    elements: Box<[mem::ManuallyDrop<T>; SEGMENT_CAPACITY]>,
 }
 
 impl<T> Segment<T> {
-  unsafe fn new() -> Segment<T> {
-    Segment {
-      len: 0,
-      elements: Box::new(mem::uninitialized()),
+    unsafe fn new() -> Segment<T> {
+        Segment {
+            len: 0,
+            elements: Box::new(mem::uninitialized()),
+        }
     }
-  }
 
-  fn is_full(&self) -> bool {
-    self.len >= SEGMENT_CAPACITY
-  }
+    fn is_full(&self) -> bool {
+        self.len >= SEGMENT_CAPACITY
+    }
 }
 
 impl<T> Drop for Segment<T> {
-  fn drop(&mut self) {
-    unsafe {
-      for element in &mut self.elements[0..self.len] {
-        mem::ManuallyDrop::drop(element);
-      }
+    fn drop(&mut self) {
+        unsafe {
+            for element in &mut self.elements[0..self.len] {
+                mem::ManuallyDrop::drop(element);
+            }
+        }
     }
-  }
 }
 
 impl<T> ops::Index<usize> for Segment<T> {
-  type Output = mem::ManuallyDrop<T>;
+    type Output = mem::ManuallyDrop<T>;
 
-  fn index(&self, index: usize) -> &<Self as ops::Index<usize>>::Output {
-    if self.len <= index {
-      panic!("index out of bounds")
+    fn index(&self, index: usize) -> &<Self as ops::Index<usize>>::Output {
+        if self.len <= index {
+            panic!("index out of bounds")
+        }
+
+        &self.elements[index]
     }
-
-    &self.elements[index]
-  }
 }
 
 impl<T> std::ops::IndexMut<usize> for Segment<T> {
-  fn index_mut(&mut self, index: usize) -> &mut <Self as ops::Index<usize>>::Output {
-    if self.len <= index {
-      panic!("index out of bounds")
-    }
+    fn index_mut(&mut self, index: usize) -> &mut <Self as ops::Index<usize>>::Output {
+        if self.len <= index {
+            panic!("index out of bounds")
+        }
 
-    &mut self.elements[index]
-  }
+        &mut self.elements[index]
+    }
 }
 
 /// A collection onto which new values can be appended, while still keeping references to previous
@@ -90,160 +90,160 @@ impl<T> std::ops::IndexMut<usize> for Segment<T> {
 /// }
 /// ```
 pub struct AppendOnlyVec<T> {
-  segments: cell::UnsafeCell<Vec<cell::UnsafeCell<Segment<T>>>>,
+    segments: cell::UnsafeCell<Vec<cell::UnsafeCell<Segment<T>>>>,
 }
 
 impl<T> AppendOnlyVec<T> {
-  /// Creates an empty `AppendOnlyVec`.
-  pub fn new() -> AppendOnlyVec<T> {
-    AppendOnlyVec {
-      segments: cell::UnsafeCell::new(vec![]),
-    }
-  }
-
-  /// Consumes a `T`, appends it to the end of the vector, and returns a reference to the newly
-  /// appended element.
-  pub fn push(&self, element: T) -> &T {
-    unsafe {
-      let last_segment = self.get_segment_with_spare_capacity();
-
-      let len = last_segment.len;
-      last_segment.len += 1;
-
-      let element_ref = &mut last_segment[len];
-      mem::replace(element_ref, mem::ManuallyDrop::new(element));
-      element_ref
-    }
-  }
-
-  /// Returns the number of elements in the vector.
-  pub fn len(&self) -> usize {
-    unsafe {
-      let segments = self.segments();
-      if let Some(last_segment) = segments.last() {
-        ((segments.len() - 1) << SEGMENT_CAPACITY_LOG_2) + (&*last_segment.get()).len
-      } else {
-        0
-      }
-    }
-  }
-
-  unsafe fn get_segment_at(&self, index: usize) -> &Segment<T> {
-    let segments = self.segments();
-    &*segments[index].get()
-  }
-
-  unsafe fn get_segment_with_spare_capacity(&self) -> &mut Segment<T> {
-    let segments = self.segments();
-    match segments.last_mut() {
-      None => self.add_segment(),
-      Some(segment) => {
-        if (*segment.get()).is_full() {
-          self.add_segment()
-        } else {
-          &mut *segment.get()
+    /// Creates an empty `AppendOnlyVec`.
+    pub fn new() -> AppendOnlyVec<T> {
+        AppendOnlyVec {
+            segments: cell::UnsafeCell::new(vec![]),
         }
-      }
     }
-  }
 
-  unsafe fn add_segment(&self) -> &mut Segment<T> {
-    let segments = self.segments();
-    segments.push(cell::UnsafeCell::new(Segment::new()));
-    let index = segments.len() - 1;
-    &mut *segments.get_unchecked_mut(index).get()
-  }
+    /// Consumes a `T`, appends it to the end of the vector, and returns a reference to the newly
+    /// appended element.
+    pub fn push(&self, element: T) -> &T {
+        unsafe {
+            let last_segment = self.get_segment_with_spare_capacity();
 
-  unsafe fn segments(&self) -> &mut Vec<cell::UnsafeCell<Segment<T>>> {
-    &mut *self.segments.get()
-  }
+            let len = last_segment.len;
+            last_segment.len += 1;
+
+            let element_ref = &mut last_segment[len];
+            mem::replace(element_ref, mem::ManuallyDrop::new(element));
+            element_ref
+        }
+    }
+
+    /// Returns the number of elements in the vector.
+    pub fn len(&self) -> usize {
+        unsafe {
+            let segments = self.segments();
+            if let Some(last_segment) = segments.last() {
+                ((segments.len() - 1) << SEGMENT_CAPACITY_LOG_2) + (&*last_segment.get()).len
+            } else {
+                0
+            }
+        }
+    }
+
+    unsafe fn get_segment_at(&self, index: usize) -> &Segment<T> {
+        let segments = self.segments();
+        &*segments[index].get()
+    }
+
+    unsafe fn get_segment_with_spare_capacity(&self) -> &mut Segment<T> {
+        let segments = self.segments();
+        match segments.last_mut() {
+            None => self.add_segment(),
+            Some(segment) => {
+                if (*segment.get()).is_full() {
+                    self.add_segment()
+                } else {
+                    &mut *segment.get()
+                }
+            }
+        }
+    }
+
+    unsafe fn add_segment(&self) -> &mut Segment<T> {
+        let segments = self.segments();
+        segments.push(cell::UnsafeCell::new(Segment::new()));
+        let index = segments.len() - 1;
+        &mut *segments.get_unchecked_mut(index).get()
+    }
+
+    unsafe fn segments(&self) -> &mut Vec<cell::UnsafeCell<Segment<T>>> {
+        &mut *self.segments.get()
+    }
 }
 
 impl<T> Default for AppendOnlyVec<T> {
-  fn default() -> Self {
-    Self::new()
-  }
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl<T> ops::Index<usize> for AppendOnlyVec<T> {
-  type Output = T;
+    type Output = T;
 
-  fn index(&self, index: usize) -> &Self::Output {
-    unsafe {
-      let segment = self.get_segment_at(index >> SEGMENT_CAPACITY_LOG_2);
-      &segment[index & SEGMENT_CAPACITY_MASK]
+    fn index(&self, index: usize) -> &Self::Output {
+        unsafe {
+            let segment = self.get_segment_at(index >> SEGMENT_CAPACITY_LOG_2);
+            &segment[index & SEGMENT_CAPACITY_MASK]
+        }
     }
-  }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::{AppendOnlyVec, SEGMENT_CAPACITY};
-  use std::ptr;
+    use super::{AppendOnlyVec, SEGMENT_CAPACITY};
+    use std::ptr;
 
-  #[test]
-  fn it_works() {
-    let vec = AppendOnlyVec::<String>::new();
-    let s1 = vec.push("hello".into());
-    let s2 = vec.push("bye".into());
-    assert_eq!(&String::from("hello"), s1);
-    assert_eq!(&String::from("bye"), s2);
-  }
-
-  #[test]
-  fn references_still_valid_after_another_segment_is_created() {
-    let vec = AppendOnlyVec::<String>::new();
-    let mut references = Vec::<&String>::new();
-    for i in 0..(SEGMENT_CAPACITY + 1) {
-      references.push(vec.push(format!("{}", i)));
+    #[test]
+    fn it_works() {
+        let vec = AppendOnlyVec::<String>::new();
+        let s1 = vec.push("hello".into());
+        let s2 = vec.push("bye".into());
+        assert_eq!(&String::from("hello"), s1);
+        assert_eq!(&String::from("bye"), s2);
     }
 
-    assert_eq!(&"0", &references[0]);
-    assert!(ptr::eq(&vec[0], references[0]));
-    assert_eq!(
-      format!("{}", SEGMENT_CAPACITY).as_str(),
-      references[SEGMENT_CAPACITY].as_str()
-    );
-  }
+    #[test]
+    fn references_still_valid_after_another_segment_is_created() {
+        let vec = AppendOnlyVec::<String>::new();
+        let mut references = Vec::<&String>::new();
+        for i in 0..(SEGMENT_CAPACITY + 1) {
+            references.push(vec.push(format!("{}", i)));
+        }
 
-  #[test]
-  fn index() {
-    let vec = AppendOnlyVec::<String>::new();
-    vec.push("hello".into());
-    vec.push("bye".into());
-
-    assert_eq!(vec[0], "hello");
-    assert_eq!(vec[1], "bye");
-  }
-
-  #[test]
-  #[should_panic]
-  fn index_out_of_bounds() {
-    let vec = AppendOnlyVec::<String>::new();
-    vec.push("hello".into());
-    vec.push("bye".into());
-    &vec[2];
-  }
-
-  #[test]
-  fn len_empty() {
-    let vec = AppendOnlyVec::<String>::new();
-    assert_eq!(0, vec.len())
-  }
-
-  #[test]
-  fn len_1() {
-    let vec = AppendOnlyVec::<String>::new();
-    vec.push("hello".into());
-    assert_eq!(1, vec.len())
-  }
-
-  #[test]
-  fn len_multiple_segments() {
-    let vec = AppendOnlyVec::<String>::new();
-    for i in 0..(SEGMENT_CAPACITY + 1) {
-      vec.push(format!("{}", i));
+        assert_eq!(&"0", &references[0]);
+        assert!(ptr::eq(&vec[0], references[0]));
+        assert_eq!(
+            format!("{}", SEGMENT_CAPACITY).as_str(),
+            references[SEGMENT_CAPACITY].as_str()
+        );
     }
-    assert_eq!(33, vec.len())
-  }
+
+    #[test]
+    fn index() {
+        let vec = AppendOnlyVec::<String>::new();
+        vec.push("hello".into());
+        vec.push("bye".into());
+
+        assert_eq!(vec[0], "hello");
+        assert_eq!(vec[1], "bye");
+    }
+
+    #[test]
+    #[should_panic]
+    fn index_out_of_bounds() {
+        let vec = AppendOnlyVec::<String>::new();
+        vec.push("hello".into());
+        vec.push("bye".into());
+        &vec[2];
+    }
+
+    #[test]
+    fn len_empty() {
+        let vec = AppendOnlyVec::<String>::new();
+        assert_eq!(0, vec.len())
+    }
+
+    #[test]
+    fn len_1() {
+        let vec = AppendOnlyVec::<String>::new();
+        vec.push("hello".into());
+        assert_eq!(1, vec.len())
+    }
+
+    #[test]
+    fn len_multiple_segments() {
+        let vec = AppendOnlyVec::<String>::new();
+        for i in 0..(SEGMENT_CAPACITY + 1) {
+            vec.push(format!("{}", i));
+        }
+        assert_eq!(33, vec.len())
+    }
 }
--- src/lazy_array.rs
+++ src/lazy_array.rs
@@ -28,62 +28,62 @@ use std::cell;
 pub struct LazyArray<T>(cell::UnsafeCell<Box<[Option<T>]>>);
 
 impl<T> LazyArray<T> {
-  pub fn new(size: usize) -> LazyArray<T> {
-    let mut init = Vec::<Option<T>>::with_capacity(size);
-    for _ in 0..size {
-      init.push(None)
+    pub fn new(size: usize) -> LazyArray<T> {
+        let mut init = Vec::<Option<T>>::with_capacity(size);
+        for _ in 0..size {
+            init.push(None)
+        }
+        LazyArray(cell::UnsafeCell::new(init.into_boxed_slice()))
     }
-    LazyArray(cell::UnsafeCell::new(init.into_boxed_slice()))
-  }
 
-  pub fn get_or_insert(&self, index: usize, t: T) -> &T {
-    (&mut unsafe { &mut *self.0.get() }[index]).get_or_insert(t)
-  }
-
-  pub fn get(&self, index: usize) -> Option<&T> {
-    if let Some(ref element) = unsafe { &*self.0.get() }[index] {
-      Some(element)
-    } else {
-      None
+    pub fn get_or_insert(&self, index: usize, t: T) -> &T {
+        (&mut unsafe { &mut *self.0.get() }[index]).get_or_insert(t)
     }
-  }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        if let Some(ref element) = unsafe { &*self.0.get() }[index] {
+            Some(element)
+        } else {
+            None
+        }
+    }
 }
 
 impl<T> Default for LazyArray<T> {
-  fn default() -> Self {
-    Self::new(0)
-  }
+    fn default() -> Self {
+        Self::new(0)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::LazyArray;
+    use super::LazyArray;
 
-  #[test]
-  fn it_works() {
-    let lazy_array = LazyArray::<i32>::new(10);
-    for i in 0..10 {
-      assert_eq!(lazy_array.get(i), None)
+    #[test]
+    fn it_works() {
+        let lazy_array = LazyArray::<i32>::new(10);
+        for i in 0..10 {
+            assert_eq!(lazy_array.get(i), None)
+        }
+
+        assert_eq!(lazy_array.get_or_insert(7, 112233), &112233);
+
+        for i in 0..10 {
+            assert_eq!(lazy_array.get(i), if i == 7 { Some(&112233) } else { None })
+        }
     }
 
-    assert_eq!(lazy_array.get_or_insert(7, 112233), &112233);
-
-    for i in 0..10 {
-      assert_eq!(lazy_array.get(i), if i == 7 { Some(&112233) } else { None })
+    #[test]
+    fn cannot_insert_twice() {
+        let lazy_array = LazyArray::<i32>::new(10);
+        assert_eq!(lazy_array.get_or_insert(7, 112233), &112233);
+        assert_eq!(lazy_array.get_or_insert(7, 445566), &112233);
     }
-  }
 
-  #[test]
-  fn cannot_insert_twice() {
-    let lazy_array = LazyArray::<i32>::new(10);
-    assert_eq!(lazy_array.get_or_insert(7, 112233), &112233);
-    assert_eq!(lazy_array.get_or_insert(7, 445566), &112233);
-  }
-
-  #[test]
-  #[should_panic]
-  fn cannot_put_out_of_bounds() {
-    let lazy_array = LazyArray::<i32>::new(10);
-    lazy_array.get_or_insert(10, 112233);
-  }
+    #[test]
+    #[should_panic]
+    fn cannot_put_out_of_bounds() {
+        let lazy_array = LazyArray::<i32>::new(10);
+        lazy_array.get_or_insert(10, 112233);
+    }
 }
